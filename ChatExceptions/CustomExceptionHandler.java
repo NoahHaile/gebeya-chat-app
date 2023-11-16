77b41848d38301bcb4b23f0c44e63f36803faeec
Kidus Berhanu <70487091+Kinn7@users.noreply.github.com>
2023-11-16 19:42:20 +0300

    Update CustomExceptionHandler.java

--- ChatExceptions/CustomExceptionHandler.java
+++ ChatExceptions/CustomExceptionHandler.java
@@ -1,3 +1,9 @@
+/**
+* Custom Exception handles an exception suited for file management which is IOExceptionn
+* and write the message of the error to a file CUSTOM_LOG_FILE.
+*
+* @author  Kidus Berhanu
+*/
 package ChatExceptions;
 
 import java.io.FileWriter;
